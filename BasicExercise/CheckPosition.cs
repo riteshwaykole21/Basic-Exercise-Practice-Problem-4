53d84e5b2c2b7ff8caddf46bb7b4a8f9afd03e68
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-05-01 10:01:37 +0530

    [Ritesh]add : Check String and Remove Specific Word Of Given String

---
+++ BasicExercise/CheckPosition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicExercise
+{
+    public class CheckPosition
+    {
+        public void Number()
+        {
+            string Name = "PHP Tutorial";
+            Console.WriteLine((Name.Substring(1 ,2).Equals("HP") ? Name.Remove(1,2) : Name));
+            
+        }
+    }
+}
---
+++ BasicExercise/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 : Check HP appears At Second Position");
+            Console.WriteLine("Enter A Option");
+            int Option = Convert.ToInt32(Console.ReadLine());
+            switch (Option)
+            {
+                case 1:
+                    CheckPosition checkPosition = new CheckPosition();
+                    checkPosition.Number();
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
