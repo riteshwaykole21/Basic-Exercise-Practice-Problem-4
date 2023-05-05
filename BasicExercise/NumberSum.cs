c96b4432f43836b7515ee753fd1bd77f57654444
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-05-05 15:46:56 +0530

    [Ritesh]add : Smallest Two Number Addition in Array

---
+++ BasicExercise/NumberSum.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicExercise
+{
+    public class NumberSum
+    {
+        public void Number(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                     if (arr[i] >  arr[j])
+                     {
+                        int temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;  
+                     }
+                }
+            }
+            int num = 0;
+            foreach (int iteam in arr)
+            {
+                num++;
+                if (iteam >= 0)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine($"The Sum Of Two Min Number is {arr[num] + arr[num - 1]}");
+        }
+    }
+}
--- BasicExercise/Program.cs
+++ BasicExercise/Program.cs
@@ -6,11 +6,12 @@ using System.Threading.Tasks;
 
 namespace BasicExercise
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("1 : Check HP appears At Second Position");
+            Console.WriteLine("2 :  Get A New string");
             Console.WriteLine("Enter A Option");
             int Option = Convert.ToInt32(Console.ReadLine());
             switch (Option)
@@ -18,6 +19,15 @@ namespace BasicExercise
                 case 1:
                     CheckPosition checkPosition = new CheckPosition();
                     checkPosition.Number();
+                    break;
+                case 2:
+                    NumberSum numberSum = new NumberSum();
+                    int[] arr = { 19, -5, -42, 2, 77 };
+                    int[] Arr1 = { 10, 34, 10, 235, 556};
+                    int[] Arr2 = { 10, 12, 39, 34 };
+                    numberSum.Number(arr);
+                    numberSum.Number(Arr1);
+                    numberSum.Number(Arr2);
                     break;
             }
             Console.ReadLine();
